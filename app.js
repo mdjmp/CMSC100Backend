0793dca1df1619f5c7286be92efd181696e23d57
mdjmp <mmperez10@up.edu.ph>
2021-07-05 15:28:31 +0800

    refactor: modularize routing mechanism

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fastify = require('fastify');
+const {routes} = require('./routes');
 
 /**
  * This is the funct call to initialize server
@@ -6,26 +7,11 @@ const fastify = require('fastify');
  * @param {{logger: boolean, trustProxy:boolean}} opts 
  * @returns {*}
  */
-
 exports.build = async (opts = { logger: true, trustProxy: true}) => {
     //initialize server using fastify
     const app =  fastify(opts);
 
-    //access root address = http://localhost/
-    app.get('/', {
-        //object
-        /**
-         * 
-         * @param {*} req - req param sent by client
-         */
-        handler: async (req) => {
-            console.log('hello world!!!');
-
-            //reponse in json format
-            return {success: true}
-        }
-    });
+    routes(app);
 
     return app;
-
 };
---
+++ routes/index.js
@@ -0,0 +1,18 @@
+/**
+ * initialize all routes
+ * @param {*} app 
+ */
+
+exports.routes = (app) => {
+    app.get('/', {
+        /**
+         * handles request for a given route
+         */
+        handler: async (req) => {
+            console.log('hello world!!!');
+
+            //reponse in json format
+            return {success: true}
+        }
+    });
+}
