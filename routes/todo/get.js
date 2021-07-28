64795c002429237c62607509853210ccd2bb618f
mdjmp <mmperez10@up.edu.ph>
2021-07-28 23:41:55 +0800

    add schema to all routes

--- routes/todo/get.js
+++ routes/todo/get.js
@@ -1,4 +1,6 @@
-const {Todo} = require('../../db');
+const { Todo } = require('../../db');
+const { definitions } = require('../../definitions');
+const { GetOneTodoResponse, GetOneTodoParams } = definitions;
 
 /**
  * Gets one todo
@@ -6,31 +8,44 @@ const {Todo} = require('../../db');
  * @param {*} app
  */
 exports.get = app => {
-  /**
-   * This gets one todos from the database give a unique ID
-   *
-   * @param {import('fastify').FastifyRequest} request
-   * @param {import('fastify').FastifyReply<Response>} response
-   */
-  app.get('/todo/:id',async (request,response) => {
-    const { params } = request;
-    const { id } = params;
 
-    const data = await Todo.findOne({id}).exec();
+  app.get('/todo/:id', {
+    schema: {
+      description: 'Get one todo',
+      tags: ['Todo'],
+      summary: 'Get one todo',
+      params: GetOneTodoParams,
+      response: {
+        200: GetOneTodoResponse
+      }
+    },
 
-    if(!data) {
+    /**
+     * This gets one todos from the database give a unique ID
+     *
+     * @param {import('fastify').FastifyRequest} request
+     * @param {import('fastify').FastifyReply<Response>} response
+     */
+    handler: async (request, response) => {
+      const { params } = request;
+      const { id } = params;
+
+      const data = await Todo.findOne({ id }).exec();
+
+      if (!data) {
         return response
-            .code(404)
-            .send({
-                success: false,
-                code: 'todo/not-found',
-                message: 'Todo doesnt exist'
-            });
-    }
+          .code(404)
+          .send({
+            success: false,
+            code: 'todo/not-found',
+            message: 'Todo doesn\'t exist'
+          });
+      }
 
-    return {
-      success: true,
-      data
-    };
+      return {
+        success: true,
+        data
+      };
+    }
   });
 };
