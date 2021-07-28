ffd9ce7de449bbd5aba456183cde7761c83d15a3
mdjmp <mmperez10@up.edu.ph>
2021-07-28 23:22:45 +0800

    add fastify swagger initialization

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const fastify = require('fastify');
+const swagger = require('fastify-swagger');
 const { routes } = require('./routes');
 const {connect} = require('./db');
+const {definitions} = require('./definitions');
+const {name:title, description, version} = require('./package.json');
 
 /**
  * This is the function to call to initialize the server
@@ -11,6 +14,22 @@ const {connect} = require('./db');
 exports.build = async (opts = { logger: false, trustProxy: false }) => {
   // initialize our server using Fastify
   const app = fastify(opts);
+
+  app.register(swagger, {
+    routePrefix: '/docs',
+    exposeRoute: true,
+    swagger: {
+      info: {
+        title,
+        description,
+        version
+      },
+      schemes: ['http', 'https'],
+      consumes: ['application/json'],
+      produces: ['application/json'],
+      definitions
+    }
+  })
 
   await connect();
   
