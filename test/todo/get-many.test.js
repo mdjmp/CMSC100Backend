1624ee43aa4d8ba615adc274a104fdf827630184
mdjmp <mmperez10@up.edu.ph>
2021-07-28 23:42:51 +0800

    add test with endDate

--- test/todo/get-many.test.js
+++ test/todo/get-many.test.js
@@ -1,6 +1,5 @@
 const { delay } = require('../../lib/delay');
 const { mongoose, Todo } = require('../../db');
-
 const { build } = require('../../app');
 require('should');
 require('tap').mochaGlobals();
@@ -33,20 +32,20 @@ describe('For the route for getting many todos GET: (/todo)', () => {
   });
 
   after(async () => {
-    // clean up the database'
-    for(const id of ids){
-      await Todo.findOneAndDelete({id});
+    // clean up the database
+    for (const id of ids) {
+      await Todo.findOneAndDelete({ id });
     }
 
     await mongoose.connection.close();
   });
 
- // happy path
- it('it should return { success: true, data: array of todos } and has a status code of 200 when called using GET and has a default limit of 3 items', async () => {
-  const response = await app.inject({
-    method: 'GET',
-    url: '/todo'
-  });
+  // happy path
+  it('it should return { success: true, data: array of todos } and has a status code of 200 when called using GET and has a default limit of 3 items', async () => {
+    const response = await app.inject({
+      method: 'GET',
+      url: '/todo'
+    });
 
     const payload = response.json();
     const { statusCode } = response;
@@ -65,7 +64,7 @@ describe('For the route for getting many todos GET: (/todo)', () => {
       } = await Todo
         .findOne({ id })
         .exec();
-  
+
       text.should.equal(textDatabase);
       done.should.equal(doneDatabase);
     }
@@ -87,14 +86,14 @@ describe('For the route for getting many todos GET: (/todo)', () => {
 
     for (const todo of data) {
       const { text, done, id } = todo;
-  
+
       const {
         text: textDatabase,
         done: doneDatabase
       } = await Todo
         .findOne({ id })
         .exec();
-  
+
       text.should.equal(textDatabase);
       done.should.equal(doneDatabase);
     }
@@ -160,10 +159,9 @@ describe('For the route for getting many todos GET: (/todo)', () => {
   it('it should return { success: true, data: array of todos } and has a status code of 200 when called using GET and has a default limit of 3 items and it should be in descending order where the last item is updated on or after startDate', async () => {
     const id = ids[parseInt(Math.random() * ids.length)];
 
-    const {dateUpdated: startDate} = await Todo
-    .findOne({id})
-    .exec();
-
+    const { dateUpdated: startDate } = await Todo
+      .findOne({ id })
+      .exec();
 
     const response = await app.inject({
       method: 'GET',
@@ -187,5 +185,36 @@ describe('For the route for getting many todos GET: (/todo)', () => {
 
     // the last data should be equal to the picked id
     data[data.length - 1].id.should.equal(id);
+  });
+
+  it('it should return { success: true, data: array of todos } and has a status code of 200 when called using GET and has a default limit of 3 items and it should be in descending order where the last item is updated on or before endDate', async () => {
+    const id = ids[parseInt(Math.random() * ids.length)];
+
+    const { dateUpdated: endDate } = await Todo
+      .findOne({ id })
+      .exec();
+
+    const response = await app.inject({
+      method: 'GET',
+      url: `/todo?endDate=${endDate}`
+    });
+
+    const payload = response.json();
+    const { statusCode } = response;
+    const { success, data } = payload;
+
+    success.should.equal(true);
+    statusCode.should.equal(200);
+    (data.length <= 3).should.equal(true);
+
+    for (let i = 0; i < data.length - 1; i++) {
+      const prevTodo = data[i];
+      const nextTodo = data[i + 1];
+
+      (nextTodo.dateUpdated < prevTodo.dateUpdated).should.equal(true);
+    }
+
+    // the last data should be equal to the picked id
+    data[0].id.should.equal(id);
   });
 });
