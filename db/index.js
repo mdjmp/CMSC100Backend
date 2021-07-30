15430b81c5479295a4c63624abc0aafe53ab0e7a
mdjmp <mmperez10@up.edu.ph>
2021-07-30 22:29:56 +0800

    add user model in database

--- db/index.js
+++ db/index.js
@@ -1,21 +1,21 @@
 const mongoose = require('mongoose');
 
-
-//setup a connection configuration to the mongodb instance
+// setup a connection configuration to the mongodb instance
 mongoose.connect('mongodb://localhost:27017/todo-cmsc100', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 /**
  * This creates a connection to the database
- * @returns  {Promise}
+ * @returns {Promise}
  */
 exports.connect = () => new Promise((resolve, reject) => {
-    const {connection} = mongoose;
-    connection.on('error', reject);
-    connection.once('open', resolve);
-})
+  const { connection } = mongoose;
+  connection.on('error', reject);
+  connection.once('open', resolve);
+});
 
 exports.Todo = require('./models/todo')(mongoose);
+exports.User = require('./models/user')(mongoose);
 exports.mongoose = mongoose;
