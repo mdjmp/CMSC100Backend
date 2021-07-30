47a66e8f9f587d85c77c1e0133627db70202d6af
mdjmp <mmperez10@up.edu.ph>
2021-07-30 22:28:41 +0800

    add user request and response

--- definitions/index.js
+++ definitions/index.js
@@ -3,135 +3,183 @@ const UniqueIDData = {
     description: 'A unique identifier',
     value: '49e89682-b62f-4ad0-a66d-375c54986555',
     example: '49e89682-b62f-4ad0-a66d-375c54986555'
-};
-
-const TextData = {
+  };
+  
+  const UsernameData = {
     type: 'string',
-    description: 'A textual string',
+    description: 'A unique username',
+    value: 'jenperez',
+    example: 'jenperez'
+  };
+  
+  const PasswordData = {
+    type: 'string',
+    description: 'Password string',
+    value: '49e89682b62f4ad0a66d375c54986555',
+    example: '49e89682b62f4ad0a66d375c54986555'
+  }
+  
+  const TextData = {
+    type: 'string',
+    description: 'Any textual string',
     value: 'Hello world!',
     example: 'Hello world!'
-};
-
-const DoneData = {
+  };
+  
+  const DoneData = {
     type: 'boolean',
     description: 'If a todo object is tagged as done',
     value: true,
     example: true
-};
-
-const DateData = {
+  };
+  
+  const DateData = {
     type: 'number',
     description: 'A date value that is in Unix Epoch',
-    value: 1627377285957,
-    example: 1627377285957
-};
-
-const SuccessData = {
+    value: 1626280628699,
+    example: 1626280628699
+  };
+  
+  const SuccessData = {
     type: 'boolean',
     description: 'State of a response',
     value: true,
     example: true
-};
-
-const LimitData = {
+  };
+  
+  const LimitData = {
     type: 'number',
     description: 'Limit of how many items we should query',
     value: 4,
     example: 4
-};
-
-const SuccessResponse = {
+  };
+  
+  const SuccessResponse = {
     type: 'object',
     description: 'Response with a success state only',
     properties: {
-        success: SuccessData
+      success: SuccessData
     }
-};
-
-const TodoFullData = {
+  };
+  
+  const TodoFullData = {
     type: 'object',
     description: 'Todo object data coming from the database',
     properties: {
-        id: UniqueIDData,
-        text: TextData,
-        done: DoneData,
-        dateUpdated: DateData,
-        dateCreated: DateData
+      id: UniqueIDData,
+      text: TextData,
+      done: DoneData,
+      dateUpdated: DateData,
+      dateCreated: DateData
     }
-};
-
-const TodoListData = {
+  };
+  
+  const TodoListData = {
     type: 'array',
     description: 'A list of todos',
     items: TodoFullData
-};
-
-const GetManyTodoQuery = {
+  }
+  
+  const GetManyTodoQuery = {
     type: 'object',
-    description: 'Query parameters for getting may todos',
+    description: 'Query parameters for getting many todos',
     properties: {
-        limit: LimitData,
-        startDate: DateData,
-        endDate: DateData
+      limit: LimitData,
+      startDate: DateData,
+      endDate: DateData
     }
-};
-
-const GetOneTodoParams = {
+  };
+  
+  const GetOneTodoParams = {
     type: 'object',
-    description: 'Parameters for getting one todos',
+    description: 'Parameter for getting one todos',
     properties: {
-        id: UniqueIDData,
-
+      id: UniqueIDData
     }
-};
-
-const GetManyTodoResponse = {
+  }
+  
+  const UserFullData = {
+    type: 'object',
+    description: 'User data for response without the password',
+    properties: {
+      username: UsernameData,
+      dateUpdated: DateData,
+      dateCreated: DateData
+    }
+  }
+  
+  const GetOneUserResponse = {
+    type: 'object',
+    description: 'Returns a a user',
+    required: ['success', 'data'],
+    properties: {
+      success: SuccessData,
+      data: UserFullData
+    }
+  }
+  
+  const GetManyTodoResponse = {
     type: 'object',
     description: 'Returns a list of todos',
-    required:['success', 'data'],
+    required: ['success', 'data'],
     properties: {
-        success: SuccessData,
-        data: TodoListData
+      success: SuccessData,
+      data: TodoListData
     }
-};
-
-const PostTodoRequest = {
+  }
+  
+  const PostUserRequest = {
+    type: 'object',
+    description: 'User object data for creation',
+    required: [
+      'username',
+      'password'
+    ],
+    properties: {
+      username: UsernameData,
+      password: PasswordData
+    }
+  }
+  
+  const PostTodoRequest = {
     type: 'object',
     description: 'Todo object data for creation',
     required: [
-        'text'
+      'text'
     ],
     properties: {
-        text: TextData,
-        done: DoneData
+      text: TextData,
+      done: DoneData
     }
-};
-
-const PutTodoRequest = {
+  }
+  
+  const PutTodoRequest = {
     type: 'object',
     description: 'Todo object data for update',
     properties: {
-        text: TextData,
-        done: DoneData
+      text: TextData,
+      done: DoneData
     }
-};
-
-const GetOneTodoResponse = {
+  }
+  
+  const GetOneTodoResponse = {
     type: 'object',
-    description: 'Returns todo',
-    required:['success', 'data'],
+    description: 'Returns a todo',
+    required: ['success', 'data'],
     properties: {
-        success: SuccessData,
-        data: TodoFullData
+      success: SuccessData,
+      data: TodoFullData
     }
-};
-
-exports.definitions = {
+  }
+  
+  exports.definitions = {
     SuccessResponse,
     GetManyTodoResponse,
     GetManyTodoQuery,
     GetOneTodoParams,
-    PostTodoRequest,
     GetOneTodoResponse,
-    PutTodoRequest
-}
+    PostTodoRequest,
+    PutTodoRequest,
+    PostUserRequest,
+    GetOneUserResponse
+  }  
